324b0536c7e32965644d4a25a2c733c9293f6f8c
ElizabethSobiya <ellizabethsobiya@gmail.com>
2024-05-06 16:18:23 +0530

    Indian rupee

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -85,58 +85,152 @@ function JobListingCards({ jobListings, loading, fetchJobListings, filters }) {
   };
 
   return (
-    <Grid container spacing={3} justifyContent="center" style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '1200px', paddingLeft: '10px', paddingRight: '10px' }}>
-    {filteredJobListings.map((job, index) => (
-      <Grid item xs={12} sm={6} md={4} key={index}>
-        <Card sx={{ borderRadius: 5, height: '100%', width: '350px', margin: '10px' }}>
-          <CardContent style={{ padding: '20px' }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <img src={job.logoUrl} alt={job.companyName} style={{ width: 60, height: 60, borderRadius: '50%', marginRight: 10 }} />
-              <div>
-                <Typography variant="h4" component="div">{job.companyName}</Typography>
-                <Typography variant="h6" component="div">{job.location.toUpperCase()}</Typography> {/* Capitalize location */}
+    <Grid
+      container
+      spacing={3}
+      justifyContent="center"
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: "1200px",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+      }}
+    >
+      {filteredJobListings.map((job, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          <Card
+            sx={{
+              borderRadius: 5,
+              height: "100%",
+              width: "350px",
+              margin: "10px",
+            }}
+          >
+            <CardContent style={{ padding: "20px" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src={job.logoUrl}
+                  alt={job.companyName}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: "50%",
+                    marginRight: 10,
+                  }}
+                />
+                <div>
+                  <Typography variant="h4" component="div">
+                    {job.companyName}
+                  </Typography>
+                  <Typography variant="h6" component="div">
+                    {job.location.toUpperCase()}
+                  </Typography>{" "}
+                  {/* Capitalize location */}
+                </div>
               </div>
-            </div>
-            {job.minJdSalary !== null && job.maxJdSalary !== null && (
-              <Typography variant="body2" component="div" style={{ marginTop: 10 }}>
-                Salary Range: {job.minJdSalary} - {job.maxJdSalary} LPA {" "}
-                <span role="img" aria-label="Green Tick">✅</span>
+              {job.minJdSalary !== null && job.maxJdSalary !== null && (
+                <Typography
+                  variant="body2"
+                  component="div"
+                  style={{ marginTop: 10 }}
+                >
+                  Estimated Salary: ₹{job.minJdSalary} - {job.maxJdSalary} LPA{" "}
+                  <span role="img" aria-label="Green Tick">
+                    ✅
+                  </span>
+                </Typography>
+              )}
+              {job.jobDetailsFromCompany && (
+                <>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    style={{ marginTop: 10, fontWeight: "bold" }}
+                  >
+                    About the Company:
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    style={{ marginTop: 5, fontWeight: "bold" }}
+                  >
+                    About Us
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    style={{ marginTop: 5 }}
+                  >
+                    {truncateDescription(job.jobDetailsFromCompany)}
+                    {job.jobDetailsFromCompany.length > 25 && (
+                      <span
+                        style={{ cursor: "pointer", color: "blue" }}
+                        onClick={() => openJobModal(job)}
+                      >
+                        View More
+                      </span>
+                    )}
+                  </Typography>
+                </>
+              )}
+              <Typography
+                variant="body2"
+                component="div"
+                style={{ marginTop: 5 }}
+              >
+                Minimum Experience <br /> {job.minExp} years
               </Typography>
-            )}
-            {job.jobDetailsFromCompany && (
-              <>
-                <Typography variant="body2" component="div" style={{ marginTop: 10, fontWeight: 'bold' }}>
-                  About the Company:
-                </Typography>
-                <Typography variant="body2" component="div" style={{ marginTop: 5, fontWeight: 'bold' }}>
-                  About Us
-                </Typography>
-                <Typography variant="body2" component="div" style={{ marginTop: 5 }}>
-                  {truncateDescription(job.jobDetailsFromCompany)}
-                  {job.jobDetailsFromCompany.length > 25 && (
-                    <span style={{ cursor: "pointer", color: 'blue' }} onClick={() => openJobModal(job)}>
-                      View More
-                    </span>
-                  )}
-                </Typography>
-              </>
-            )}
-            <Typography variant="body2" component="div" style={{ marginTop: 5, }}>
-                 Minimum Experience <br /> {job.minExp} years
-                </Typography>
-          </CardContent>
-          <CardActions style={{display:'flex', flexDirection:'column', justifyContent: 'space-between', marginTop: 'auto', padding: '20px' }}>
-            <Button variant="contained"  style={{ margin: 5,}} fullWidth color="primary"  startIcon={<span role="img" aria-label="Thunder">⚡️</span>}>Easy Apply</Button>
-            <Button variant="contained" style={{ marginTop: 10,}} fullWidth color="secondary"  endIcon={<span role="img" aria-label="Unlock">🔓</span>}>Unlock Referral</Button>
-          </CardActions>
-        </Card>
-      </Grid>
-    ))}
-    <div ref={loaderRef} style={{ marginTop: '20px', marginBottom: '20px', textAlign: 'center' }}> {/* Add margin top and bottom for the loader */}
-      {loading && <CircularProgress />}
-    </div>
-    <JobModal open={openModal} onClose={closeJobModal} job={selectedJob} />
-  </Grid>  
+            </CardContent>
+            <CardActions
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                marginTop: "auto",
+                padding: "20px",
+              }}
+            >
+              <Button
+                variant="contained"
+                style={{ margin: 5 }}
+                fullWidth
+                color="primary"
+                startIcon={
+                  <span role="img" aria-label="Thunder">
+                    ⚡️
+                  </span>
+                }
+              >
+                Easy Apply
+              </Button>
+              <Button
+                variant="contained"
+                style={{ marginTop: 10 }}
+                fullWidth
+                color="secondary"
+                endIcon={
+                  <span role="img" aria-label="Unlock">
+                    🔓
+                  </span>
+                }
+              >
+                Unlock Referral
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+      <div
+        ref={loaderRef}
+        style={{ marginTop: "20px", marginBottom: "20px", textAlign: "center" }}
+      >
+        {" "}
+        {/* Add margin top and bottom for the loader */}
+        {loading && <CircularProgress />}
+      </div>
+      <JobModal open={openModal} onClose={closeJobModal} job={selectedJob} />
+    </Grid>
   );
 }
 
@@ -158,7 +252,8 @@ function JobModal({ open, onClose, job }) {
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{job.jobRole.toUpperCase()}</DialogTitle> {/* Capitalize modal title */}
+      <DialogTitle>{job.jobRole.toUpperCase()}</DialogTitle>{" "}
+      {/* Capitalize modal title */}
       <DialogContent>
         <Typography>{jobDetailsFromCompany}</Typography>
       </DialogContent>
